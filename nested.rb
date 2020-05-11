1a6bff6d27976d182e06bd71e7ef07c6d28c567d
Nicole Wang <nicole.wang@yale.edu>
2020-05-11 18:47:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,6 +132,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:programmer_hash] 
+programmer_hash[:dennis_ritchie][:programmer_hash] << "Assembly"
 programmer_hash
 end
